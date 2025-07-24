7d3496aa34fa92a6868258e49bcd01be92360f13
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-24 10:01:24 +0200

    final-repo-commit

--- src/main/java/com/backend/Focus_Flow/repository/LabelRepository.java
+++ src/main/java/com/backend/Focus_Flow/repository/LabelRepository.java
@@ -1,2 +1,9 @@
-package com.backend.Focus_Flow.repository;public interface LabelRepository {
+package com.backend.Focus_Flow.repository;
+
+import com.backend.Focus_Flow.model.Label;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LabelRepository extends JpaRepository <Label, Long> {
 }
--- src/main/java/com/backend/Focus_Flow/repository/ProjectRepository.java
+++ src/main/java/com/backend/Focus_Flow/repository/ProjectRepository.java
@@ -1,2 +1,9 @@
-package com.backend.Focus_Flow.repository;public class ProjectRepository {
+package com.backend.Focus_Flow.repository;
+
+import com.backend.Focus_Flow.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository <Project, Long> {
 }
