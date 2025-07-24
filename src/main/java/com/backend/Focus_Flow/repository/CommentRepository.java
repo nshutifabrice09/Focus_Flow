1dde8b6f6d54ce332fa51f91e270a9db54e32309
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-24 09:43:15 +0200

    repo-updates-commit

--- src/main/java/com/backend/Focus_Flow/repository/CommentRepository.java
+++ src/main/java/com/backend/Focus_Flow/repository/CommentRepository.java
@@ -1,2 +1,9 @@
-package com.backend.Focus_Flow.repository;public class CommentRepository {
+package com.backend.Focus_Flow.repository;
+
+import com.backend.Focus_Flow.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository <Comment, Long> {
 }
