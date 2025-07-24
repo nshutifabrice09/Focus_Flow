7ac5b9d5a6cef851d59f4c13a6b3755ea11c615b
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-24 10:07:35 +0200

    entitiesannotation-updates-commit

--- src/main/java/com/backend/Focus_Flow/repository/UserRepository.java
+++ src/main/java/com/backend/Focus_Flow/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.backend.Focus_Flow.repository;public interface UserRepository {
+package com.backend.Focus_Flow.repository;
+
+import com.backend.Focus_Flow.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
