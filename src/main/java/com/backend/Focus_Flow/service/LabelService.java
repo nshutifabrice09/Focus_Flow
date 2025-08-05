e6b441f98ddaa670cdbaa6d020afaa07dbb74497
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-05 09:41:08 +0200

    initial-labelServiceImpl-commit

--- src/main/java/com/backend/Focus_Flow/service/LabelService.java
+++ src/main/java/com/backend/Focus_Flow/service/LabelService.java
@@ -1,2 +1,14 @@
-package com.backend.Focus_Flow.service;public interface LabelService {
+package com.backend.Focus_Flow.service;
+
+import com.backend.Focus_Flow.model.Label;
+
+import java.util.List;
+
+public interface LabelService {
+
+    Label saveLabel(Label label, Long taskId);
+    List<Label> getAllLabels();
+    Label getLabel(Long id);
+    Label updateLabel(Long id, Label label);
+    void removeLabel(Long id);
 }
