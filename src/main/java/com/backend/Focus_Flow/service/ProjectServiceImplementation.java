412c090d9bfc79501ee048a6f9ad1c17abe4478e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-05 11:35:50 +0200

    final-projectServiceImpl-commit

--- src/main/java/com/backend/Focus_Flow/service/ProjectServiceImplementation.java
+++ src/main/java/com/backend/Focus_Flow/service/ProjectServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.backend.Focus_Flow.service;public class ProjectServiceImplementation {
+package com.backend.Focus_Flow.service;
+
+import com.backend.Focus_Flow.model.Project;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectServiceImplementation implements ProjectService{
+    @Override
+    public Project saveProject(Project project, Long createdBy, Long memberId, Long taskId) {
+        return null;
+    }
+
+    @Override
+    public List<Project> getAllProjects() {
+        return null;
+    }
+
+    @Override
+    public Project getProject(Long id) {
+        return null;
+    }
+
+    @Override
+    public Project updateProject(Long id, Project project) {
+        return null;
+    }
+
+    @Override
+    public void removeProject(Long id) {
+
+    }
 }
