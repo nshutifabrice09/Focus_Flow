7e2688fad524fa763fe7cd755708565c4622ff8e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-05 09:09:06 +0200

    final-commentService-commit

--- src/main/java/com/backend/Focus_Flow/service/CommentService.java
+++ src/main/java/com/backend/Focus_Flow/service/CommentService.java
@@ -1,2 +1,8 @@
-package com.backend.Focus_Flow.service;public interface CommentService {
+package com.backend.Focus_Flow.service;
+
+import com.backend.Focus_Flow.model.Comment;
+
+public interface CommentService {
+
+    Comment saveComment(Comment comment,)
 }
