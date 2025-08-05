e133c96a1a2a8bfb178482e2b4379a082594db07
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-05 09:44:02 +0200

    final-labelServiceImpl-commit

--- src/main/java/com/backend/Focus_Flow/service/LabelServiceImplementation.java
+++ src/main/java/com/backend/Focus_Flow/service/LabelServiceImplementation.java
@@ -1,2 +1,35 @@
-package com.backend.Focus_Flow.service;public class LabelServiceImplementation {
+package com.backend.Focus_Flow.service;
+
+import com.backend.Focus_Flow.model.Label;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LabelServiceImplementation implements LabelService{
+
+    @Override
+    public Label saveLabel(Label label, Long taskId) {
+        return null;
+    }
+
+    @Override
+    public List<Label> getAllLabels() {
+        return null;
+    }
+
+    @Override
+    public Label getLabel(Long id) {
+        return null;
+    }
+
+    @Override
+    public Label updateLabel(Long id, Label label) {
+        return null;
+    }
+
+    @Override
+    public void removeLabel(Long id) {
+
+    }
 }
