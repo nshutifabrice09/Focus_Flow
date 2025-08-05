49695447f6dd02ea0731556906b5584715a859c7
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-05 09:10:31 +0200

    final-update-commentService-commit

--- src/main/java/com/backend/Focus_Flow/service/CommentService.java
+++ src/main/java/com/backend/Focus_Flow/service/CommentService.java
@@ -2,7 +2,14 @@ package com.backend.Focus_Flow.service;
 
 import com.backend.Focus_Flow.model.Comment;
 
+import java.util.List;
+
 public interface CommentService {
 
-    Comment saveComment(Comment comment,)
+    Comment saveComment(Comment comment,Long authorId, Long taskId);
+    List<Comment> getComments();
+    Comment getComment(Long id);
+    Comment updateComment(Long id, Comment comment);
+    void removeComment(Long id);
+
 }
