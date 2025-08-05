b3f91815ec6107db57b4ec9480b6018879770522
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-05 10:57:20 +0200

    initial-projectServiceImpl-commit

--- src/main/java/com/backend/Focus_Flow/service/ProjectService.java
+++ src/main/java/com/backend/Focus_Flow/service/ProjectService.java
@@ -1,2 +1,13 @@
-package com.backend.Focus_Flow.service;public interface ProjectService {
+package com.backend.Focus_Flow.service;
+
+import com.backend.Focus_Flow.model.Project;
+
+import java.util.List;
+
+public interface ProjectService {
+    Project saveProject(Project project, Long createdBy, Long memberId, Long taskId);
+    List<Project> getAllProjects();
+    Project getProject(Long id);
+    Project updateProject(Long id, Project project);
+    void removeProject(Long id);
 }
---
+++ src/main/java/com/backend/Focus_Flow/service/ProjectServiceImplementation.java
@@ -0,0 +1,2 @@
+package com.backend.Focus_Flow.service;public class ProjectServiceImplementation {
+}
