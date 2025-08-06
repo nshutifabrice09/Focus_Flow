a02fd3f8360cf9cbd0d8148692ceb2869a5115ce
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-06 11:37:22 +0200

    initial-UserServiceImpl-commit

--- src/main/java/com/backend/Focus_Flow/service/UserService.java
+++ src/main/java/com/backend/Focus_Flow/service/UserService.java
@@ -1,2 +1,13 @@
-package com.backend.Focus_Flow.service;public interface UserService {
+package com.backend.Focus_Flow.service;
+
+import com.backend.Focus_Flow.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    List<User> getAllUsers();
+    User getUser(Long id);
+    User updateUser(Long id, User user);
+    void removeUser(Long id);
 }
