cd41571dc913176c7db58318298932c07f4c2bc6
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-06 12:17:48 +0200

    updates-controller-commit

--- src/main/java/com/backend/Focus_Flow/controller/CommentController.java
+++ src/main/java/com/backend/Focus_Flow/controller/CommentController.java
@@ -1,2 +1,18 @@
-package com.backend.Focus_Flow.controller;public class CommentController {
+package com.backend.Focus_Flow.controller;
+
+import com.backend.Focus_Flow.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
 }
