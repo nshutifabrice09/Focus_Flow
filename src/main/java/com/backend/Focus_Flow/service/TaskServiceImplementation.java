bf2f612af6b230146f26d556355f564972e0bf9a
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-06 11:13:26 +0200

    final-taskServiceImpl-commiy

--- src/main/java/com/backend/Focus_Flow/service/TaskServiceImplementation.java
+++ src/main/java/com/backend/Focus_Flow/service/TaskServiceImplementation.java
@@ -1,2 +1,53 @@
-package com.backend.Focus_Flow.service;public class TaskServiceImplentation {
+package com.backend.Focus_Flow.service;
+
+import com.backend.Focus_Flow.model.Task;
+import com.backend.Focus_Flow.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskServiceImplementation implements TaskService{
+
+    private final TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+    private final LabelRepository labelRepository;
+    private final CommentRepository commentRepository;
+
+
+    @Autowired
+    public TaskServiceImplementation(TaskRepository taskRepository, ProjectRepository projectRepository, UserRepository userRepository, LabelRepository labelRepository, CommentRepository commentRepository) {
+        this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
+        this.labelRepository = labelRepository;
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public Task saveTask(Task task, Long projectId, Long assignedTo, Long createdBy, Long labelId, Long commentId) {
+        return null;
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return null;
+    }
+
+    @Override
+    public Task getTask(Long id) {
+        return null;
+    }
+
+    @Override
+    public Task updateTask(Long id, Task task) {
+        return null;
+    }
+
+    @Override
+    public void removeTask(Long id) {
+
+    }
 }
