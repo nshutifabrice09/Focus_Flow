ffb99bcf45c0066810e740c924b0ddc6d97f61f0
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-06 10:29:24 +0200

    initial-taskServiceImpl-commiy

--- src/main/java/com/backend/Focus_Flow/service/TaskService.java
+++ src/main/java/com/backend/Focus_Flow/service/TaskService.java
@@ -2,6 +2,12 @@ package com.backend.Focus_Flow.service;
 
 import com.backend.Focus_Flow.model.Task;
 
+import java.util.List;
+
 public interface TaskService {
-    Task saveTask(Task task, )
+    Task saveTask(Task task, Long projectId, Long assignedTo, Long createdBy, Long labelId, Long commentId);
+    List<Task> getAllTasks();
+    Task getTask(Long id);
+    Task updateTask(Long id, Task task);
+    void removeTask(Long id);
 }
