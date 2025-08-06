f36b1da957effec6c99805f43b853d731e35a0d5
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-06 11:41:50 +0200

    final-UserServiceImpl-commit

--- src/main/java/com/backend/Focus_Flow/service/UserServiceImplementation.java
+++ src/main/java/com/backend/Focus_Flow/service/UserServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.backend.Focus_Flow.service;public class UserServiceImplementation {
+package com.backend.Focus_Flow.service;
+
+import com.backend.Focus_Flow.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImplementation implements UserService{
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        return null;
+    }
+
+    @Override
+    public void removeUser(Long id) {
+
+    }
 }
