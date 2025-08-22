aa4c047fe7aa7034659c3111d5a03770cf4bd5dd
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-22 11:13:09 +0200

    final-userController-update-commit

--- src/main/java/com/backend/Focus_Flow/controller/TaskController.java
+++ src/main/java/com/backend/Focus_Flow/controller/TaskController.java
@@ -1,9 +1,12 @@
 package com.backend.Focus_Flow.controller;
 
+import com.backend.Focus_Flow.model.Task;
 import com.backend.Focus_Flow.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Path;
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -14,5 +17,32 @@ public class TaskController {
     @Autowired
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
+    }
+
+    @PostMapping("/task/{projectId}/{assignedTo}/{createdBy}/{labelId}/{commentId}")
+    public Task save(@RequestBody Task task,
+                     @PathVariable ("projectId") Long projectId,@PathVariable("assignedTo") Long assignedTo, @PathVariable("createdBy") Long createdBy,
+                     @PathVariable("labelId") Long labelId, @PathVariable ("commentId") Long commentId){
+        return taskService.saveTask(task, projectId, assignedTo, createdBy, labelId, commentId);
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> taskList(){
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/task/{id}")
+    public Task getTask(@PathVariable ("id") Long id){
+        return taskService.getTask(id);
+    }
+
+    @PutMapping("/update/task/{id}")
+    public Task updateTask(@PathVariable ("id") Long id, @RequestBody Task task){
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/delete/task/{id}")
+    public void deleteTask(@PathVariable ("id") Long id){
+        taskService.removeTask(id);
     }
 }
