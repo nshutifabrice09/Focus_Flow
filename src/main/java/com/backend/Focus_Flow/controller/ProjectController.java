a61e8182c8d5065056d2d60ab5580d4222f23346
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-22 10:55:33 +0200

    final -projectController-commit

--- src/main/java/com/backend/Focus_Flow/controller/ProjectController.java
+++ src/main/java/com/backend/Focus_Flow/controller/ProjectController.java
@@ -1,2 +1,20 @@
-package com.backend.Focus_Flow.controller;public class ProjectController {
+package com.backend.Focus_Flow.controller;
+
+import com.backend.Focus_Flow.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    
 }
