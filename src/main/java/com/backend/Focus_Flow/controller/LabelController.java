905977c15a761ab7fd2ff89511e0fdf1309c2698
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-22 10:39:09 +0200

    initial-projectController-commit

--- src/main/java/com/backend/Focus_Flow/controller/LabelController.java
+++ src/main/java/com/backend/Focus_Flow/controller/LabelController.java
@@ -1,9 +1,46 @@
 package com.backend.Focus_Flow.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.backend.Focus_Flow.model.Label;
+import com.backend.Focus_Flow.service.LabelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class LabelController {
+
+    private final LabelService labelService;
+
+    @Autowired
+    public LabelController(LabelService labelService) {
+        this.labelService = labelService;
+    }
+
+    @PostMapping("/label/{taskId}")
+    public Label save (@RequestBody Label label, @PathVariable ("taskId") Long taskId){
+        return labelService.saveLabel(label, taskId);
+    }
+
+    @GetMapping("/labels")
+    public List<Label> labelList(){
+        return labelService.getAllLabels();
+    }
+
+    @GetMapping("/label/{id}")
+    public Label getLabel(@PathVariable ("id") Long id){
+        return labelService.getLabel(id);
+    }
+
+    @PutMapping("/update/label/{id}")
+    public Label updateLabel(@PathVariable ("id") Long id, @RequestBody Label label){
+        return labelService.updateLabel(id, label);
+    }
+
+    @DeleteMapping("/delete/label/{id}")
+    public void deleteLabel(@PathVariable ("id") Long id){
+        labelService.removeLabel(id);
+    }
+
 }
